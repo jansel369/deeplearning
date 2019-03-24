4cbbab211597c5f85bd2c1d9ab27fa40d8220212
jansel369 <dianzel555@gmail.com>
2019-03-24 22:10:42 +0800

    add divider data utils

--- src/test.py
+++ src/test.py
@@ -1,10 +1,13 @@
 import torch as pt
 import lib.logistic_regression as lr
 from utils.loader import *
+import utils.data_divider import *
 
 device = pt.device("cuda") if pt.cuda.is_available() else pt.device("cpu")
 
 banknote = loader("banknote.csv", device)
+
+X_train, Y_train, X_test, Y_test = divide_data(banknote)
 
 print(banknote["Y"])
 print(banknote["X"])
---
+++ src/utils/data_divider.py
@@ -0,0 +1,13 @@
+
+def divide_data(dict, percentage = 0.2):
+    X = dict["X"]
+    Y = dict["Y"]
+    m = dict["m"]
+    test_count = round(m * percentage)
+
+    X_test = X[0:test_count, :]
+    Y_test = Y[:, 0:test_count]
+    X_train = X[test_count:m, :]
+    Y_train = Y[:, test_count:m]
+
+    return X_train, Y_train, X_test, Y_test
